9206dff9bf2f4afe5709a3a1e442af7ae43e4eb7
Surya <stk222@ucsd.edu>
2024-11-14 00:49:37 -0800

    modified hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Autonomous Mobile Robots",  "CUDA Programming", "Robot Perception", "Machine Learning", "Visual SLAM"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
